f533d1584dc655143cf9d9c5997776965bc04079
Ian Jarvis <ijarvis12@gmail.com>
2021-07-15 09:11:05 -0500

    updated to have decimal objects and misc
    misc:  changed 'garbage' variables to '_'

--- differentiate.py
+++ differentiate.py
@@ -4,6 +4,7 @@
 ## program differentiates a user inputed function ##
 ##                                                ##
 from math import *
+from decimal import Decimal
 
 print("")
 print("This program finds the differential of a function using three-point derviative")
@@ -22,38 +23,31 @@ f = lambda x: eval(function)
 # get x value to differentiate on
 x = input("Enter a value x: ")
 
-# if user input, attempt to make 'x' a float, else exit
+# if user input, attempt to make 'x' a decimal object, else exit
 if len(x) > 0:
     try:
-        x = float(x)
+        x = Decimal(x)
     except:
         print("Bad input")
-        garbage = input("Press <Enter> to end program")
+        _ = input("Press <Enter> to end program")
         exit()
 else:
     exit()
 
 # try evaluating the function at x, if no good exit
 try:
-    garbage = f(x)
+    _ = f(x)
 except:
     print("Bad function or f(x)")
     garbage = input("Press <Enter> to end program")
     exit()
 
-# make variable h as small as possible float given what limitations computers can do with them
-if abs(x) >= 1e+16 or (abs(x) <= 1e-16 and x != 0):
-    digs, e = str(x).split('e')
-    h = float("0.000000000000001e"+e)
-else:
-    left, right = str(x).split('.')
-    h = 1
-    for d in range(15-len(left)):
-        h /= 10
+# make step variable h as small as conveniently possible
+h = Decimal('0.000000000000000000000000001')
 
 # differentiate
 y = (1/(2*h))*(f(x-2*h) - 4*f(x-h) + 3*f(x))
 
 # ouput result
 print("The differential of",function,"at x="+str(x),"is",y)
-garbage = input("Press <Enter> to end program")
+_ = input("Press <Enter> to end program")
